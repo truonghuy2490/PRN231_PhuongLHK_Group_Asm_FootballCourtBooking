43a4c3c54661a8145665efa446f9cf62cd0c1da6
Nguyen Vu Truong Huy <nguyenvutruonghuy112@gmail.com>
2024-10-23 00:18:17 +0700

    save db

--- FBS.BusinessObjects/obj/Release/net8.0/FBS.BusinessObjects.AssemblyInfo.cs
+++ FBS.BusinessObjects/obj/Release/net8.0/FBS.BusinessObjects.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FBS.BusinessObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b02277516075b2b6431b121fc48fb90482841b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5806c041357d64bef5ea0766a8e39aa553016bc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FBS.BusinessObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FBS.BusinessObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
